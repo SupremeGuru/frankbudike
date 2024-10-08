ac2b6d404382981bbec340b65b62333d73dd2451
emma-ezeilo <ezeiloemmanuel7@gmail.com>
2024-10-08 20:54:56 +0100

    reconfiguring the routes fixed the footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React ,{useState} from 'react'
-import { imgAssets } from '../assets/images/imgAssets'
+import { imgAssets, imgAssets2 } from '../assets/images/imgAssets'
 import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     const [visible, setVisible] = useState(false);
   return (
-    <div className="fixed z-10 top-[10px] md:top-[30px] w-full px-[16px] md:px-[40px]">
+    <div className="fixed z-10 top-[10px] md:top-[30px] w-full px-[6px] md:px-[40px]">
         <div className=" fixed backdrop-filter backdrop-blur-lg flex items-center justify-between container py-[24px] px-[24px] bg-[#00000080] rounded-[40px] font-inter font-medium">
             {/* <div className="flex justify-between items-center shrink-0"> */}
-                <Link className="text-20 text-white" to='/frankbudike/'>Frank Studios</Link>
+                <Link className="text-20 text-white" to='/'>Frank Studios</Link>
             {/* </div> */}
             <div id="" className="hidden md:flex lg:flex justify-between text-16 text-white px-[40px]">
                 <div className=''></div>
@@ -18,7 +18,7 @@ const Navbar = () => {
                 <a href='#gallery'>
                     <p className="capitalize cursor-pointer py-[10px] lg:py-2 max-lg:border-b border-b-stroke p-3">Gallery</p>
                 </a>
-                <NavLink to="biography">
+                <NavLink to="/biography">
                     <p className="text-16 font-inter font-medium py-[10px] lg:py-2 max-lg:border-b border-b-stroke p-3">Biography</p>
                 </NavLink>
                 <div className="flex items-center gap-6">
@@ -39,7 +39,7 @@ const Navbar = () => {
             <div className={`absolute sm:flex md:flex lg:flex top-0 right-0 bottom-0 overflow-hidden transition-all  ${visible ? 'w-full':'w-0'}`}>
                 <div className={`flex flex-col text-white-600`}>
                     <div onClick={()=>setVisible(false)} className="flex items-center">
-                        <img src={imgAssets.logo} width="30" className="h-4 rotate-180" alt="menu"/>
+                        <img src={imgAssets2.logo} width="30" className="h-4 rotate-180" alt="menu"/>
                         <p>Back</p>
                     </div>
 
