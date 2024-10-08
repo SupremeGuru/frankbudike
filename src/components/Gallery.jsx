a7c64cd218dac299a9e981ffd2caa8ac790efef3
emma-ezeilo <ezeiloemmanuel7@gmail.com>
2024-10-08 21:55:28 +0100

    reconfiguring the routes fixed the footer 5

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import ArtCollection from './ArtCollection'
 
 const Gallery = () => {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React ,{useState} from 'react'
+import React ,{useState} from 'react';
 import { imgAssets, imgAssets2 } from '../assets/images/imgAssets'
 import { Link, NavLink } from 'react-router-dom'
 
